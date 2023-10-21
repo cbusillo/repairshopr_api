ee12a4b0f18fce28c93d3baa91264e22db89e854
Chris Busillo <info@shinycomputers.com>
2023-10-20 21:01:55 -0400

    change to Optional

--- repairshopr_api/models/product.py
+++ repairshopr_api/models/product.py
@@ -1,38 +1,35 @@
 from dataclasses import dataclass, field
+from repairshopr_api.base.model import BaseModel
 
 
 @dataclass
-class Product:
+class Product(BaseModel):
     id: int
-    price_cost: float = 0
-    price_retail: float = 0
-    condition: str = ""
-    description: str = ""
-    maintain_stock: bool = False
-    name: str = ""
-    quantity: int = 0
-    warranty: str = ""
-    sort_order: str = ""
-    reorder_at: str = ""
-    disabled: bool = False
-    taxable: bool = False
-    product_category: str = ""
-    category_path: str = ""
-    upc_code: str = ""
-    discount_percent: str = ""
-    warranty_template_id: str = ""
-    qb_item_id: str = ""
-    desired_stock_level: str = ""
-    price_wholesale: float = 0
-    notes: str = ""
-    tax_rate_id: str = ""
-    physical_location: str = ""
-    serialized: bool = False
+    price_cost: float = None
+    price_retail: float = None
+    condition: str = None
+    description: str = None
+    maintain_stock: bool = None
+    name: str = None
+    quantity: int = None
+    warranty: str = None
+    sort_order: str = None
+    reorder_at: str = None
+    disabled: bool = None
+    taxable: bool = None
+    product_category: str = None
+    category_path: str = None
+    upc_code: str = None
+    discount_percent: str = None
+    warranty_template_id: str = None
+    qb_item_id: str = None
+    desired_stock_level: str = None
+    price_wholesale: float = None
+    notes: str = None
+    tax_rate_id: str = None
+    physical_location: str = None
+    serialized: bool = None
     vendor_ids: list[int] = field(default=list)
-    long_description: str = ""
+    long_description: str = None
     location_quantities: list[dict] = field(default=list)
     photos: list[dict] = field(default=list)
-
-    @classmethod
-    def from_dict(cls, data: dict):
-        return cls(**data)
