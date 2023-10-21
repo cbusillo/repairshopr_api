3f953d2d8b8cf1d9373b5059af44f6dac263b2aa
Chris Busillo <info@shinycomputers.com>
2023-10-21 02:20:32 -0400

    add payment model

--- repairshopr_api/models/__init__.py
+++ repairshopr_api/models/__init__.py
@@ -1,6 +1,7 @@
 from .product import Product
 from .ticket import Ticket, Comment
 from .user import User
-from .invoice import Invoice
+from .invoice import Invoice, InvoiceLine
 from .customer import Customer, Contact
 from .estimate import Estimate
+from .payment import Payment
