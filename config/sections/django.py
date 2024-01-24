32af9012be899f98aa26104b2728a1ccde2ea57f
Chris Busillo <info@shinycomputers.com>
2024-01-23 19:33:32 -0500

    add mariadb to settings

--- config/sections/django.py
+++ config/sections/django.py
@@ -6,3 +6,8 @@ from config.serializable import Serializable
 class Django(Serializable):
     secret_key: str = ""
     last_updated_at: datetime | None = None
+    db_engine: str = "mysql"
+    db_host: str = "localhost"
+    db_name: str = "repairshopr"
+    db_user: str = "repairshopr_api"
+    db_password: str = ""
--- repairshopr_sync/repairshopr_sync/settings.py
+++ repairshopr_sync/repairshopr_sync/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = "repairshopr_sync.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": f"django.db.backends.{settings.django.db_engine}",
+        "HOST": settings.django.db_host,
+        "NAME": settings.django.db_name,
+        "USER": settings.django.db_user,
+        "PASSWORD": settings.django.db_password,
     }
 }
 
