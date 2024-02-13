f82aa2b8e64c38282395ea1713823597ea579089
Chris Busillo <info@shinycomputers.com>
2024-02-13 11:41:28 -0500

    add migrations to scripts

--- run_django.py
+++ run_django.py
@@ -1,9 +1,19 @@
-from pathlib import Path
+# django_manage.py
 import subprocess
 
 def runserver():
-    manage_py_path = Path(__file__).parent / "repairshopr_sync/manage.py"
-    try:
-        subprocess.run(["python", manage_py_path.as_posix(), "runserver"], check=True)
-    except subprocess.CalledProcessError as e:
-        print(f"Error running the Django server: {e}")
+    makemigrations()
+    migrate()
+    subprocess.run(["python", "repairshopr_sync/manage.py", "runserver"])
+
+def makemigrations():
+    subprocess.run(["python", "repairshopr_sync/manage.py", "makemigrations"])
+
+def migrate():
+    subprocess.run(["python", "repairshopr_sync/manage.py", "migrate"])
+
+def import_from_repairshopr():
+    makemigrations()
+    migrate()
+    subprocess.run(["python", "repairshopr_sync/manage.py", "import_from_repairshopr"])
+
